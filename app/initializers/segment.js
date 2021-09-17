6ceaa25925f3983b4dcd7dc4ddc4bbd66c65ddf7
Camille TJHOA <camille.tjhoa@concordnow.com>
2021-09-17 17:48:36 +0000

    Avoid config usage on build time

--- app/initializers/segment.js
+++ app/initializers/segment.js
@@ -7,6 +7,15 @@ export function initialize() {
   const { environment = 'development' } = config;
   const segmentConfig = { segment, environment };
 
+  let segmentDomain = config.segment.proxyDomain || 'https://cdn.segment.com/';
+  if (!/\/$/.test(segmentDomain)) {
+    segmentDomain += '/';
+  }
+
+  window['__ember-cli-segment__'] = {};
+  window['__ember-cli-segment__'].WRITE_KEY = config.segment.WRITE_KEY;
+  window['__ember-cli-segment__'].host = segmentDomain;
+
   application.register('config:segment', segmentConfig, { instantiate: false });
   application.inject('service:segment', 'config', 'config:segment');
 }
